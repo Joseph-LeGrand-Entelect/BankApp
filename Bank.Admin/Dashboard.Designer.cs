038ebabf03b08d237f2c7906b05424bae9a1ac20
Kenneth Mangwane <48716000+kenneth-mangwane@users.noreply.github.com>
2022-06-07 19:27:31 +0200

    Added some proffesional looking stuff

--- Bank.Admin/Dashboard.Designer.cs
+++ Bank.Admin/Dashboard.Designer.cs
@@ -38,11 +38,19 @@ namespace Bank.Admin
             this.label3 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accountsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.businessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 139);
+            this.button1.Location = new System.Drawing.Point(79, 175);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(186, 38);
             this.button1.TabIndex = 0;
@@ -54,7 +62,7 @@ namespace Bank.Admin
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(79, 28);
+            this.label1.Location = new System.Drawing.Point(79, 85);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(221, 37);
             this.label1.TabIndex = 1;
@@ -62,7 +70,7 @@ namespace Bank.Admin
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(330, 272);
+            this.button2.Location = new System.Drawing.Point(320, 320);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(186, 69);
             this.button2.TabIndex = 2;
@@ -72,7 +80,7 @@ namespace Bank.Admin
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(79, 272);
+            this.button3.Location = new System.Drawing.Point(79, 320);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(186, 69);
             this.button3.TabIndex = 3;
@@ -84,7 +92,7 @@ namespace Bank.Admin
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(79, 95);
+            this.label2.Location = new System.Drawing.Point(79, 138);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(163, 21);
             this.label2.TabIndex = 5;
@@ -92,7 +100,7 @@ namespace Bank.Admin
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(79, 229);
+            this.textBox1.Location = new System.Drawing.Point(79, 262);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(186, 23);
             this.textBox1.TabIndex = 6;
@@ -100,7 +108,7 @@ namespace Bank.Admin
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(79, 198);
+            this.label3.Location = new System.Drawing.Point(79, 229);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(114, 15);
             this.label3.TabIndex = 7;
@@ -108,7 +116,7 @@ namespace Bank.Admin
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(330, 229);
+            this.textBox2.Location = new System.Drawing.Point(320, 262);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(186, 23);
             this.textBox2.TabIndex = 8;
@@ -116,17 +124,69 @@ namespace Bank.Admin
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(330, 198);
+            this.label4.Location = new System.Drawing.Point(320, 229);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(94, 15);
             this.label4.TabIndex = 9;
             this.label4.Text = "Deposit Amount";
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(540, 82);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 10;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.accountsToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // homeToolStripMenuItem
+            // 
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.homeToolStripMenuItem.Text = "Home";
+            // 
+            // accountsToolStripMenuItem
+            // 
+            this.accountsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientsToolStripMenuItem,
+            this.businessToolStripMenuItem});
+            this.accountsToolStripMenuItem.Name = "accountsToolStripMenuItem";
+            this.accountsToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.accountsToolStripMenuItem.Text = "Accounts";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // clientsToolStripMenuItem
+            // 
+            this.clientsToolStripMenuItem.Name = "clientsToolStripMenuItem";
+            this.clientsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.clientsToolStripMenuItem.Text = "Personal";
+            // 
+            // businessToolStripMenuItem
+            // 
+            this.businessToolStripMenuItem.Name = "businessToolStripMenuItem";
+            this.businessToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.businessToolStripMenuItem.Text = "Business";
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
@@ -136,10 +196,14 @@ namespace Bank.Admin
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Dashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dashboard";
             this.Load += new System.EventHandler(this.Dashboard_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -156,6 +220,13 @@ namespace Bank.Admin
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accountsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem businessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
     }
 }
 
